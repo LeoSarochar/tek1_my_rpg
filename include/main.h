7a8d65bb5cef091519445d8873d89b2dc23a0b3f
Elchurross <louis.boulet@epitech.eu>
2020-03-21 10:22:15 +0100

    init main.h

---
+++ include/main.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2019
+** .H
+** File description:
+** call all .h
+*/
+
+#ifndef MY_MAIN_H_
+#define MY_MAIN_H_
+
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+#include <SFML/Window/Export.h>
+#include <SFML/Window/Context.h>
+#include <SFML/Window.h>
+#include <SFML/Graphics/Rect.h>
+#include <SFML/Window/Window.h>
+#include <SFML/Graphics/Export.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#endif //MY_MAIN_H_
