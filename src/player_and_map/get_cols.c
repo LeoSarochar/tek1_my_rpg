e69fd85e8d5bf234685987de6b372026037e8f27
Léo Sarochar <leo.sarochar@epitech.eu>
2020-05-03 23:29:49 +0200

    Fixing compile error

--- src/player_and_map/get_cols.c
+++ src/player_and_map/get_cols.c
@@ -39,7 +39,6 @@ char *read_file(struct stat *f_info, char *name)
     char *buff = NULL;
     int fd = open_file(name);
     int len = 0;
-    int size = 0;
 
     stat(name, f_info);
     len = f_info->st_size;
