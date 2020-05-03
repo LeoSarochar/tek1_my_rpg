7b5007d662dda0cfc9b72b5fb23ab5852ac81f03
Elchurross <louis.boulet@epitech.eu>
2020-05-03 11:55:24 +0200

    Sur la branche ui  Votre branche est à jour avec 'origin/ui'. 	init part  Modifications qui seront validées : 	modifié :         src/init.c

--- src/init.c
+++ src/init.c
@@ -7,10 +7,14 @@
 */
 
 #include "init.h"
+#include "particule.h"
+#define WIDTH 1920
+#define HEIGHT 1080
 
 void init(main_t *main_struct)
 {
     for (int i = 0; init_funcs[i].ptr != NULL; i++)
         init_funcs[i].ptr(main_struct);
     init_screen_menu(main_struct);
+    main_struct->part = create_particle((V2I){76, 76}, sfWhite);
 }
