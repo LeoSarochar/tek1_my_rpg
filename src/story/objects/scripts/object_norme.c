a34a32970f61b5443636121c724ebcb648923f48
Léo Sarochar <leo.sarochar@epitech.eu>
2020-05-03 11:15:07 +0200

    Fixing compile error

--- src/story/objects/scripts/object_norme.c
+++ src/story/objects/scripts/object_norme.c
@@ -12,7 +12,7 @@ void after_norme_book(main_t *main)
     story_t *story = main->story;
 
     story->current_object->sprite->visible = 0;
-    story->quests->text = "Retourner voir Sanchez pour\n
+    story->quests->text = "Retourner voir Sanchez pour\n\
 apprendre a coder my_putchar";
     story->quests->quest_id = 3;
 }
